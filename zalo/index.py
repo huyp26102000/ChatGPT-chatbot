ef2ba1771517a87f6d82ce2639169aded1333e2d
hanatashiyuna <nhattiennguyen224@gmail.com>
2023-04-10 17:00:28 +0700

    feature/zalo-update-push-message

--- zalo/index.py
+++ zalo/index.py
@@ -47,62 +47,62 @@ def zalowebhook():
         print('received')
         return '200 OK HTTPS.', 200
 
-    # if(data['sender']['id'] == '4356460878800442485'):
-    try:
-        message = data['message']['text']
-        sender_id = data['sender']['id']
-        for quest in question:
-            if (message == quest):
-                # request to send message here
-                # response = requests.post('https://openapi.zalo.me/v2.0/oa/message', json='body').json()
-                return '200 OK HTTPS.', 200
+    if(data['sender']['id'] == '4356460878800442485'):
+        try:
+            message = data['message']['text']
+            sender_id = data['sender']['id']
+            for quest in question:
+                if (message == quest):
+                    # request to send message here
+                    # response = requests.post('https://openapi.zalo.me/v2.0/oa/message', json='body').json()
+                    return '200 OK HTTPS.', 200
 
-        gpt3 = gpt.zalo()
-        msg = {
-            "recipient": {
-                "user_id": sender_id
-            },
-            "message": {
-                "text": gpt3.get_msg_from_gpt(appconfig['gpt_api_key'], message),
-                # "text": message,
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "buttons": [
-                            {
-                                "title": "Rút ngắn đoạn văn",
-                                "type": "oa.query.show",
-                                "payload": "Rút ngắn đoạn văn"
-                            },
-                            {
-                                "title": "Motivation quote",
-                                "type": "oa.query.hide",
-                                "payload": "Motivation quote"
-                            },
-                            {
-                                "title": "Thông tin tuyển sinh",
-                                "type": "oa.query.show",
-                                "payload": "Thông tin tuyển sinh"
-                            },
-                        ]
+            gpt3 = gpt.zalo()
+            msg = {
+                "recipient": {
+                    "user_id": sender_id
+                },
+                "message": {
+                    "text": gpt3.get_msg_from_gpt(appconfig['gpt_api_key'], message),
+                    # "text": message,
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "buttons": [
+                                {
+                                    "title": "Rút ngắn đoạn văn",
+                                    "type": "oa.query.show",
+                                    "payload": "Rút ngắn đoạn văn"
+                                },
+                                {
+                                    "title": "Motivation quote",
+                                    "type": "oa.query.show",
+                                    "payload": "Motivation quote"
+                                },
+                                {
+                                    "title": "Thông tin tuyển sinh",
+                                    "type": "oa.query.show",
+                                    "payload": "Thông tin tuyển sinh"
+                                },
+                            ]
+                        }
                     }
                 }
             }
-        }
-        header = {
-            'access_token': appconfig["access_token"]
-        }
-        response = requests.post('https://openapi.zalo.me/v2.0/oa/message', headers=header, json=msg)
-        if response.status_code == 200:
-            pass
-        else:
-            print('Lỗi khi gửi tin nhắn: ', response.content)
+            header = {
+                'access_token': appconfig["access_token"]
+            }
+            response = requests.post('https://openapi.zalo.me/v2.0/oa/message', headers=header, json=msg)
+            if response.status_code == 200:
+                pass
+            else:
+                print('Lỗi khi gửi tin nhắn: ', response.content)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return "Error", 500
-    # else:
-    #     print('none')
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return "Error", 500
+    else:
+        print('none')
     return '200 OK HTTPS.', 200
 
 if __name__ == '__main__':
